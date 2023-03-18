987c567d470eca9451047af74b24c492f197cc22
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-03-18 13:35:40 +0800

    Use template to make insert more readable

--- CPU/HyperCalm/HyperBloomFilter.h
+++ CPU/HyperCalm/HyperBloomFilter.h
@@ -15,7 +15,6 @@ public:
 	double time_threshold;
 	uint32_t bucket_num;
 	uint32_t seeds[TABLE_NUM + 1];
-	bool use_SIMD;
 
 	HyperBloomFilter(uint32_t memory, double time_threshold_, int seed = 123) {
 		bucket_num = memory / TABLE_NUM / sizeof(uint64_t) * TABLE_NUM;
@@ -33,64 +32,78 @@ public:
 		delete[] buckets;
 	}
 
-	bool insert(int key, double time) {
-		int first_bucket_pos = CalculatePos(key, TABLE_NUM) % bucket_num & ~(TABLE_NUM - 1);
-		bool ans = 0;
 #ifdef SIMD
-			__m512i* x = (__m512i*)(buckets + first_bucket_pos);
-			uint64_t b[8];
-			for (int i = 0; i < 8; ++i) {
-				int now_tag = int(time / time_threshold + 1.0 * i / TABLE_NUM) % 3 + 1;
-				int ban_tag = now_tag % 3;
-				b[7 - i] = MASK[ban_tag];
-			}
-			__m512i ban = _mm512_set_epi64(b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]);
-			ban = _mm512_xor_epi64(*x, ban);
-			ban = _mm512_and_epi64(_mm512_rol_epi64(ban, 63), ban);
-			ban = _mm512_and_epi64(ban, _mm512_set1_epi64(MASK[1]));
-			ban = _mm512_or_epi64(_mm512_rol_epi64(ban, 1), ban);
-			*x = _mm512_andnot_epi64(ban, *x);
-			for (int i = 0; i < TABLE_NUM; ++i) {
-				int pos = CalculatePos(key, i) % 32;
-				int bucket_pos = (first_bucket_pos + i);
-
-				int now_tag = int(time / time_threshold + 1.0 * i / TABLE_NUM) % 3 + 1;
-
-				uint64_t& x = buckets[bucket_pos];
-				int old_tag = (x >> (2 * pos)) & 3;
-				if (old_tag == 0)
-					ans = 1;
-				x += uint64_t(now_tag - old_tag) << (2 * pos);
-			}
+	template<bool use_simd = true>
 #else
-			for (int i = 0; i < TABLE_NUM; ++i) {
-				int pos = CalculatePos(key, i) % 32;
-				int bucket_pos = (first_bucket_pos + i);
-
-				int now_tag = int(time / time_threshold + 1.0 * i / TABLE_NUM) % 3 + 1;
-				int ban_tag = now_tag % 3 + 1;
-
-				uint64_t& x = buckets[bucket_pos];
-				uint64_t ban_bits = x ^ MASK[ban_tag - 1];
-				ban_bits &= ban_bits >> 1;
-				ban_bits &= MASK[1];
-				ban_bits |= ban_bits << 1;
-				x &= ~ban_bits;
-
-				int old_tag = (x >> (2 * pos)) & 3;
-				if (old_tag == 0)
-					ans = 1;
-				x += uint64_t(now_tag - old_tag) << (2 * pos);
-			}
+	template<bool use_simd = false>
 #endif
-		return ans;
-	}
+	bool insert(int key, double time);
 
 private:
 	inline uint32_t CalculatePos(uint32_t key, int i) {
 		return CalculateBucketPos(key, seeds[i]);
 	}
 };
+
+
+template<>
+bool HyperBloomFilter::insert<true>(int key, double time) {
+	int first_bucket_pos = CalculatePos(key, TABLE_NUM) % bucket_num & ~(TABLE_NUM - 1);
+	bool ans = 0;
+	__m512i* x = (__m512i*)(buckets + first_bucket_pos);
+	uint64_t b[8];
+	for (int i = 0; i < 8; ++i) {
+		int now_tag = int(time / time_threshold + 1.0 * i / TABLE_NUM) % 3 + 1;
+		int ban_tag = now_tag % 3;
+		b[7 - i] = MASK[ban_tag];
+	}
+	__m512i ban = _mm512_set_epi64(b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]);
+	ban = _mm512_xor_epi64(*x, ban);
+	ban = _mm512_and_epi64(_mm512_rol_epi64(ban, 63), ban);
+	ban = _mm512_and_epi64(ban, _mm512_set1_epi64(MASK[1]));
+	ban = _mm512_or_epi64(_mm512_rol_epi64(ban, 1), ban);
+	*x = _mm512_andnot_epi64(ban, *x);
+	for (int i = 0; i < TABLE_NUM; ++i) {
+		int pos = CalculatePos(key, i) % 32;
+		int bucket_pos = (first_bucket_pos + i);
+
+		int now_tag = int(time / time_threshold + 1.0 * i / TABLE_NUM) % 3 + 1;
+
+		uint64_t& x = buckets[bucket_pos];
+		int old_tag = (x >> (2 * pos)) & 3;
+		if (old_tag == 0)
+			ans = 1;
+		x += uint64_t(now_tag - old_tag) << (2 * pos);
+	}
+	return ans;
+}
+
+template<>
+bool HyperBloomFilter::insert<false>(int key, double time) {
+	int first_bucket_pos = CalculatePos(key, TABLE_NUM) % bucket_num & ~(TABLE_NUM - 1);
+	bool ans = 0;
+	for (int i = 0; i < TABLE_NUM; ++i) {
+		int pos = CalculatePos(key, i) % 32;
+		int bucket_pos = (first_bucket_pos + i);
+
+		int now_tag = int(time / time_threshold + 1.0 * i / TABLE_NUM) % 3 + 1;
+		int ban_tag = now_tag % 3;
+
+		uint64_t& x = buckets[bucket_pos];
+		uint64_t ban_bits = x ^ MASK[ban_tag];
+		ban_bits &= ban_bits >> 1;
+		ban_bits &= MASK[1];
+		ban_bits |= ban_bits << 1;
+		x &= ~ban_bits;
+
+		int old_tag = (x >> (2 * pos)) & 3;
+		if (old_tag == 0)
+			ans = 1;
+		x += uint64_t(now_tag - old_tag) << (2 * pos);
+	}
+	return ans;
+}
+
 
 #undef CELL_PER_BUCKET
 #undef TABLE_NUM
