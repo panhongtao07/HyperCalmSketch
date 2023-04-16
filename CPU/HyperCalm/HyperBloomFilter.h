27f2135573fd23dfe83c29402c719d524189795e
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-04-16 20:43:26 +0800

    Update bit operation

--- CPU/HyperCalm/HyperBloomFilter.h
+++ CPU/HyperCalm/HyperBloomFilter.h
@@ -70,6 +70,7 @@ private:
 
 
 bool HyperBloomFilter::insert(int key, double time) {
+	static_assert(kCellBits == 2, "Insert is specified for 2 bit cell!");
 	int first_bucket_pos = CalculatePos(key, kTableNum) % bucket_num & ~(kTableNum - 1);
 	bool ans = 0;
 	if constexpr(use_simd) {
@@ -78,44 +79,45 @@ bool HyperBloomFilter::insert(int key, double time) {
 		for (int i = 0; i < 8; ++i) {
 			int now_tag = int(time / time_threshold + 1.0 * i / kTableNum) % 3 + 1;
 			int ban_tag_m1 = now_tag % 3;
-			b[7 - i] = MASK[ban_tag_m1];
+			b[7 - i] = STATE_MASKS[ban_tag_m1];
 		}
-		__m512i ban = _mm512_set_epi64(b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]);
-		ban = _mm512_xor_epi64(*x, ban);
-		ban = _mm512_and_epi64(_mm512_rol_epi64(ban, 63), ban);
-		ban = _mm512_and_epi64(ban, _mm512_set1_epi64(ODD_BIT_MASK));
-		ban = _mm512_or_epi64(_mm512_rol_epi64(ban, 1), ban);
-		*x = _mm512_andnot_epi64(ban, *x);
+		__m512i is_ban_bits = _mm512_set_epi64(b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]);
+		is_ban_bits = _mm512_xor_epi64(is_ban_bits, *x);
+		is_ban_bits = _mm512_or_epi64(is_ban_bits, _mm512_srli_epi64(is_ban_bits, 1));
+		is_ban_bits = _mm512_and_epi64(is_ban_bits, _mm512_set1_epi64(ODD_BIT_MASK));
+		__m512i mask = _mm512_or_epi64(is_ban_bits, _mm512_slli_epi64(is_ban_bits, 1));
+		*x = _mm512_and_epi64(*x, mask);
 		for (int i = 0; i < kTableNum; ++i) {
-			int pos = CalculatePos(key, i) % kCellPerBucket;
+			int cell_pos = CalculatePos(key, i) % kCellPerBucket;
 			int bucket_pos = (first_bucket_pos + i);
 
 			int now_tag = int(time / time_threshold + 1.0 * i / kTableNum) % 3 + 1;
 
-			uint64_t& x = buckets[bucket_pos];
-			int old_tag = (x >> (2 * pos)) & 3;
+			int old_tag = (buckets[bucket_pos] >> (kCellBits * cell_pos)) & kCellMask;
 			if (old_tag == 0)
 				ans = 1;
-			x += uint64_t(now_tag - old_tag) << (2 * pos);
+			buckets[bucket_pos] ^= uint64_t(now_tag ^ old_tag) << (kCellBits * cell_pos);
 		}
 	} else {
 		for (int i = 0; i < kTableNum; ++i) {
-			int pos = CalculatePos(key, i) % kCellPerBucket;
+			int cell_pos = CalculatePos(key, i) % kCellPerBucket;
 			int bucket_pos = (first_bucket_pos + i);
 
 			int now_tag = int(time / time_threshold + 1.0 * i / kTableNum) % 3 + 1;
 			int ban_tag_m1 = now_tag % 3;
 
-			uint64_t is_ban_bits = buckets[bucket_pos] ^ MASK[ban_tag_m1];
-			is_ban_bits &= is_ban_bits >> 1;
+			uint64_t is_ban_bits = buckets[bucket_pos] ^ STATE_MASKS[ban_tag_m1];
+			// 1 = any(not same), 0 = all(same)
+			is_ban_bits |= is_ban_bits >> 1;
 			is_ban_bits &= ODD_BIT_MASK;
+			// if all(same), clear
 			uint64_t mask = is_ban_bits | (is_ban_bits << 1);
-			buckets[bucket_pos] &= ~mask;
+			buckets[bucket_pos] &= mask;
 
-			int old_tag = (buckets[bucket_pos] >> (2 * pos)) & 3;
+			int old_tag = (buckets[bucket_pos] >> (kCellBits * cell_pos)) & kCellMask;
 			if (old_tag == 0)
 				ans = 1;
-			buckets[bucket_pos] += uint64_t(now_tag - old_tag) << (2 * pos);
+			buckets[bucket_pos] ^= uint64_t(now_tag ^ old_tag) << (kCellBits * cell_pos);
 		}
 	}
 	return ans;
