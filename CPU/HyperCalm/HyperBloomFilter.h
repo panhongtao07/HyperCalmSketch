05c3b8b8bbd07c7c71d90c5a035557615cf48614
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-04-24 02:48:21 +0800

    Specify Test mode HBF

--- CPU/HyperCalm/HyperBloomFilter.h
+++ CPU/HyperCalm/HyperBloomFilter.h
@@ -19,9 +19,16 @@ static constexpr size_t kStateNum = 3;
 
 }
 
+namespace HyperBF {
+	enum CounterType {
+		None,
+		SyncWithBucket,
+		Test,
+	};
+};
 
 
-template <size_t CellBits = 2>
+template <size_t CellBits = 2, HyperBF::CounterType counterType = HyperBF::SyncWithBucket>
 class HyperBloomFilter {
 	static constexpr size_t CellPerBucket = sizeof(uint64_t) * 8 / CellBits;
 	static constexpr uint64_t CellMask = (1 << CellBits) - 1;
@@ -38,12 +45,13 @@ public:
 #else
 	static constexpr bool use_simd = false;
 #endif
-	static constexpr bool use_counter = !use_simd;
+	static constexpr bool use_counter = counterType != HyperBF::None;
 	static constexpr size_t MaxReportSize = CellMask;
 
 	HyperBloomFilter(uint32_t memory, double time_threshold, int seed = 123);
 	~HyperBloomFilter() {
 		delete[] buckets;
+		delete[] counters;
 	}
 
 	int insert_cnt(int key, double time);
@@ -56,8 +64,8 @@ private:
 };
 
 
-template <size_t CellBits>
-HyperBloomFilter<CellBits>::HyperBloomFilter(
+template <size_t CellBits, HyperBF::CounterType counterType>
+HyperBloomFilter<CellBits, counterType>::HyperBloomFilter(
 	uint32_t memory, double time_threshold, int seed
 ) : counters(nullptr), time_threshold(time_threshold) {
 	uint32_t memsize = memory / sizeof(uint64_t);
@@ -142,9 +150,78 @@ int HyperBloomFilter<2>::insert_cnt(int key, double time) {
 	return min_cnt;
 }
 
-template <size_t CellBits>
-bool HyperBloomFilter<CellBits>::insert(int key, double time) {
+template <size_t CellBits, HyperBF::CounterType counterType>
+bool HyperBloomFilter<CellBits, counterType>::insert(int key, double time) {
 	return insert_cnt(key, time) == 0;
 }
+
+
+// Test mode HBF mimics the behavior of the original HBF,
+// but use a whole 64-bit to store a cell.
+// It's used for testing the accuracy of the HBF,
+// meanwhile also useful for understanding the original algorithm.
+template <size_t CellBits>
+class HyperBloomFilter<CellBits, HyperBF::Test> {
+	static constexpr size_t CellPerBucket = sizeof(uint64_t) * 8 / CellBits;
+	static constexpr size_t TableNum = 8;
+public:
+	uint64_t* buckets;
+	uint64_t* counters;
+	double time_threshold;
+	uint32_t bucket_num;
+	uint32_t seeds[TableNum + 1];
+	static constexpr size_t MaxReportSize = CellMask;
+
+	HyperBloomFilter(uint32_t memory, double time_threshold, int seed = 123) :
+		time_threshold(time_threshold), bucket_num(memory / sizeof(uint64_t)) {
+		buckets = new (align_val_t { 64 }) uint64_t[bucket_num * CellPerBucket] {};
+		counters = new (align_val_t { 64 }) uint64_t[bucket_num * CellPerBucket] {};
+		mt19937 rng(seed);
+		for (int i = 0; i <= TableNum; ++i) {
+			seeds[i] = rng();
+		}
+		printf("d = %d\t (Number of mimic arrays in HyperBF)\n", bucket_num);
+	}
+
+	~HyperBloomFilter() {
+		delete[] buckets;
+		delete[] counters;
+	}
+
+	int insert_cnt(int key, double time) {
+		int first_bucket_pos = CalculatePos(key, TableNum) % bucket_num & ~(TableNum - 1);
+		int min_cnt = CellMask, max_cnt = 0;
+		for (int i = 0; i < TableNum; ++i) {
+			int cell_pos = CalculatePos(key, i) % CellPerBucket;
+			int bucket_pos = (first_bucket_pos + i) * CellPerBucket;
+
+			int now_tag = int(time / time_threshold + 1.0 * i / TableNum) % 3 + 1;
+			int ban_tag = now_tag % 3 + 1;
+
+			for (int j = 0; j < CellPerBucket; ++j) {
+				if (buckets[bucket_pos + j] == ban_tag_m1) {
+					buckets[bucket_pos + j] = 0;
+					counters[bucket_pos + j] = 0;
+				}
+			}
+			buckets[bucket_pos + cell_pos] = now_tag;
+			int cnt = counters[bucket_pos + cell_pos];
+			max_cnt = max(max_cnt, cnt);
+			min_cnt = min(min_cnt, cnt);
+			if (cnt != MaxReportSize) {
+				++counters[bucket_pos + cell_pos];
+			}
+		}
+		return min_cnt;
+	}
+	bool insert(int key, double time) {
+		return insert_cnt(key, time) == 0;
+	}
+
+private:
+	inline uint32_t CalculatePos(uint32_t key, int i) {
+		return (key * seeds[i]) >> 15;
+	}
+};
 
 #endif // _HYPERBLOOMFILTER_H_
